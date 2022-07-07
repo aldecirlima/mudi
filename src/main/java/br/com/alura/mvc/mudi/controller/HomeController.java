18e2d6c435664ece5d71352ab79fb1add86bdcdd
Aldecir Neves de Lima <aldecirlima@gmail.com>
2022-07-06 21:37:03 -0300

    Aula 3 até a lição 5

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,30 +1,25 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repositories.PedidoRepository;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private PedidoRepository repository;
+	
 	@GetMapping("/home")
-	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Xiaomi Redmi Note 11");
-		pedido.setUrlImagem("https://m.media-amazon.com/images/I/41kXRr2qHkL._AC_SL1000_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/dp/B09QSB4N2C?ref_=cm_sw_r_cp_ud_dp_70VR68CCCSVZMMPEVTBC");
-//		pedido.setUrlProduto("https://a.co/d/00l1aFu");
-		pedido.setDescricao("Qualquer descrição");
-		
-		List<Pedido> pedidos = Arrays.asList(pedido);
-		
+	public String home(Model model) {	
+		List<Pedido> pedidos = repository.recuperaTodosOsPedidos();
 		model.addAttribute("pedidos", pedidos);
-		
 		return "home";
 	}
 	
