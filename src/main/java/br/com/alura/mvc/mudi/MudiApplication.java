4a44e51fbf5b551e565090cda6f27c04b2b8bb8b
Aldecir Neves de Lima <aldecirlima@gmail.com>
2022-07-17 10:45:02 -0300

    Configuração para que o sistema seja empacotado com o formato WAR em produção, no eclipse, continua utilizando o servidor tomcat do Spring

--- src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
+++ src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
@@ -2,9 +2,11 @@ package br.com.alura.mvc.mudi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+// Esterder a classe SpringBootServletInitializer para que o empacotamento WAR funcione em produção.
 @SpringBootApplication
-public class MudiApplication {
+public class MudiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MudiApplication.class, args);
